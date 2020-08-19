712f7759adebe93f10d6a64b1231d8a9faf5c738
Navanjane <nkindelpitiya@gmail.com>
2020-08-19 13:54:56 +0530

    initial

---
+++ Readme.md
@@ -0,0 +1,3 @@
+clone the repository
+cd into the project directory
+**pip3 install -r requirements.txt** 
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/navanjane/New Volume/ondev projects/tution_management/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
