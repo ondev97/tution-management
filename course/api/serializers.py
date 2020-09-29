3b7593de789259d39d08cb4a0959a0b146ce67cb
Navanjane <nkindelpitiya@gmail.com>
2020-09-29 16:35:45 +0530

    nested serializer added

--- course/api/serializers.py
+++ course/api/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 from course.models import Course, Module
+from accounts.models import Teacher
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['course_name', 'author', ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["author"] = TeacherSerializer(
+            instance.author).data
+        return representation
 
 
 class ModuleSerializer(serializers.ModelSerializer):
