a5e5d3bfb690af1e4332459e2552c4d89a11c233
Navanjane <nkindelpitiya@gmail.com>
2020-09-29 15:01:40 +0530

    venev deletd from git

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/navanjane/New Volume/ondev projects/tution_management/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
